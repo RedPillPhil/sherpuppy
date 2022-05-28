6fb6b15106984c82261ddaed55264060a3ef6d9f
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-05-28 16:17:18 -0400

    Update main.47e5ad78.chunk.js

--- puppyswap/static/js/main.47e5ad78.chunk.js
+++ puppyswap/static/js/main.47e5ad78.chunk.js
@@ -1217,7 +1217,7 @@ body {
   padding: 12px;
   margin-left: 0.5rem;
   cursor: pointer;
-`;function Mi(){return r.a.createElement("a",{id:"link",href:"https://soswap.finance",target:"_blank",rel:"noreferrer"},r.a.createElement(Fi,null,r.a.createElement("img",{src:"https://sherpuppy.dog/puppyswap/static/media/logo.a1f61c89.svg",alt:"SoSwap",style:{width:"76px"}})))}n(887);var Di=n(512);const Li=[{title:"Trade",route:"swap"},{title:"",route:"bridge"},{title:"",route:"asset"},{title:"",route:"earn"},{title:"",route:"info"}],zi=ue.default.div`
+`;function Mi(){return r.a.createElement("a",{id:"link",href:"https://soswap.finance",target:"_blank",rel:"noreferrer"},r.a.createElement(Fi,null,r.a.createElement("img",{src:"https://soswap.finance/static/media/logo.a1f61c89.svg",alt:"SoSwap",style:{width:"76px"}})))}n(887);var Di=n(512);const Li=[{title:"Trade",route:"swap"},{title:"",route:"bridge"},{title:"",route:"asset"},{title:"",route:"earn"},{title:"",route:"info"}],zi=ue.default.div`
   .ant-tabs-tab-btn {
     font-size: 14px;
     font-weight: 500;
