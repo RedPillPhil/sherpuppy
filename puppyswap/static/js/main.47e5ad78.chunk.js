9ee6e1a42825e220741ce97801e16074177d309a
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-05-28 18:19:08 -0400

    Update main.47e5ad78.chunk.js

--- puppyswap/static/js/main.47e5ad78.chunk.js
+++ puppyswap/static/js/main.47e5ad78.chunk.js
@@ -232,7 +232,7 @@ html {
 
 body {
   min-height: 100vh;
-  background: linear-gradient(225deg, #BEC1FF 0%, #EDF0FB 35%, #DCF7EC 50%, #f84354 65%, #001137 100%);;
+  background: linear-gradient(225deg, #BEC1FF 0%, #EDF0FB 35%, #DCF7EC 50%, #001137 65%, #f84354 100%);;
 }
 `,Sa=ue.default.div`
   margin-right: 0.5rem;
