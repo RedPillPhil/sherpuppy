7b9008975c04fc15f30159c32007cd0815217aae
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-19 18:21:25 -0400

    Update main.ad4f2c66.chunk.js

--- Lottery/static/js/main.ad4f2c66.chunk.js
+++ Lottery/static/js/main.ad4f2c66.chunk.js
@@ -428,7 +428,7 @@
 					var j = new g.a;
 					j.setProvider(g.a.givenProvider);
 					var x = new j.eth.Contract(K, "0x51626AD5346e7Be227D8341204a0447077963481");
-					var x2 = new j.eth.Contract(K, "0x51626AD5346e7Be227D8341204a0447077963481");
+					var x2 = new j.eth.Contract(K, "0x3aC22795304A27edb04Cfe2475DCEf0c5C8B5539");
 					return Object(C.jsx)($.Provider, {
 						value: {
 							web3: u,
