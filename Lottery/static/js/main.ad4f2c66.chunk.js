f1d5a76bf106c21ec814ce864ac63f71e3ff58be
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-19 17:24:17 -0400

    Update main.ad4f2c66.chunk.js

--- Lottery/static/js/main.ad4f2c66.chunk.js
+++ Lottery/static/js/main.ad4f2c66.chunk.js
@@ -573,7 +573,7 @@
 										}
 										return B({
 											busd: 0,
-											referred: 0,
+											beans: 0,
 											rewards: 0,
 											approved: 0
 										}), e.abrupt("return");
@@ -586,7 +586,7 @@
 									case 6:
 										x = e.sent, a = Object(m.a)(x, 4), s = a[0], c = a[1], d = a[2], p = a[3], B({
 											busd: o("".concat(s)),
-											referred: c,
+											beans: c,
 											rewards: o("".concat(d)),
 											approved: p
 										}), e.next = 19;
@@ -594,7 +594,7 @@
 									case 15:
 										e.prev = 15, e.t0 = e.catch(3), console.error(e.t0), B({
 											busd: 0,
-											referred: 0,
+											beans: 0,
 											rewards: 0,
 											approved: 0
 										});
