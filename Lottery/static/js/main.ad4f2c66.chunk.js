0b4d1c6362a79782c6f7b92a829f18d5299e63b0
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-19 17:19:12 -0400

    Update main.ad4f2c66.chunk.js

--- Lottery/static/js/main.ad4f2c66.chunk.js
+++ Lottery/static/js/main.ad4f2c66.chunk.js
@@ -584,7 +584,7 @@
 											return console.error("beanrewards", e), 0
 										})), u(b)]);
 									case 6:
-										t = e.sent, a = Object(m.a)(t, 4), s = a[0], c = a[1], d = a[2], p = a[3], B({
+										x = e.sent, a = Object(m.a)(t, 4), s = a[0], c = a[1], d = a[2], p = a[3], B({
 											busd: o("".concat(s)),
 											referred: c,
 											rewards: o("".concat(d)),
@@ -779,7 +779,7 @@
 								children: "Referral Count"
 							}), Object(C.jsxs)(F.a, {
 								variant: "h5",
-								children: [T.referred, " Referred"]
+								children: [X.referred, " Referred"]
 							})]
 						}), Object(C.jsxs)(c.a, {
 							paddingTop: 4,
