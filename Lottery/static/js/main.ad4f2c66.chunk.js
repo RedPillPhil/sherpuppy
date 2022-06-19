eea3b694d91e1e3497bff244ef84f4291e8cb29a
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-18 23:37:13 -0400

    Update main.ad4f2c66.chunk.js

--- Lottery/static/js/main.ad4f2c66.chunk.js
+++ Lottery/static/js/main.ad4f2c66.chunk.js
@@ -571,7 +571,7 @@
 							return A.a.wrap((function(e) {
 								for (;;) switch (e.prev = e.next) {
 									case 0:
-										return L(!0), t = N(), a = s("".concat(k)), e.prev = 3, e.next = 6, n.methods.enter(t).send({
+										return L(!0), t = N(), a = s("".concat(k)), e.prev = 3, e.next = 6, n.methods.enter().send({
 											from: b,
 											value: 0
 										});
